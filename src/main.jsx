06e300f71d61208b8e28dbda97b97387f1a48b9d
AllefRamos14 <mariaalice6123@gmail.com>
2026-05-17 15:24:08 -0300

    feat: adiciona alternância de tema

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App";
 import { GlobalStyles } from "./styles/GlobalStyles";
+import { darkTheme } from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
 
       <App />
@@ -21,6 +23,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         pauseOnHover
         newestOnTop
       />
-    
+    </ThemeProvider>
   </React.StrictMode>
 );
